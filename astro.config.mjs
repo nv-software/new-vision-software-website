54e66891e77cdccdd669d8e91b9d2fbf24ad11b2
KaliemSB <mycael590@gmail.com>
2025-05-23 13:55:30 -0300

    fix: tailwind incorrect usage

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,15 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss(), tsconfigPaths()],
-    },
-    integrations: [{
-        name: "inject-global-styles",
-        hooks: {
-            "astro:config:setup": ({ injectScript }) => {
-                injectScript("page", "import '@/styles/tailwind.css';");
-            },
-        },
-		}, react()],
+	vite: {
+		plugins: [tailwindcss(), tsconfigPaths()],
+	},
+	integrations: [react()],
 });
